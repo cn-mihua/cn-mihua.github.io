eefc0218430c82c511a8dc90e202ca132a5bbf6d
“zhou251” <492406488@qq.com>
2021-03-06 13:24:42 +0800

    Site updated: 2021-03-06 13:24:38

---
+++ js/artitalkkey.js
@@ -0,0 +1,19 @@
+new Artitalk({
+    appId: '6z9EWOyTv1MEidxiCQW9CTR3-MdYXbMMI',  //替换为你的appId
+    appKey: 'y6CIGzNgFMl0sgU7yzL2WSXw', //替换为你的appKey
+  	pageSize: 8,
+  	color1: "#49b1f5",
+  	color2: "#00c4b6",
+    //serverURL: "https://Hg****Nq.api.lncldglobal.com",
+    //Hg****Nq替换为你的appId前八位
+    atEmoji: {
+         'baiyan':'https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/baiyan.png',
+         'bishi':'https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/bishi.png',
+         'bizui':'https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/bizui.png',} //自定义表情包配置示例
+});
+$(".power").css("padding","10px");
+$(".power").css("text-align","left");
+$("#switchUser").css("padding-right","10px");
+$("#pubShuo").css("padding-right","10px");
+$("#pubComment").css("padding-right","10px");
+$("#uploadSource").css("padding-right","10px");
